f576d7df5512fe13a8369b54eb78c306555e4d9c
Sandesh Rasal <sandeshrasal@gmail.com>
2026-09-07 11:17:59 +0530

    Added a backend url to dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -821,7 +821,7 @@ async function sendMessage() {
         // SEND QUESTION + TRANSACTIONS TO SERVER
         // ==========================================
 
-        const response = await fetch("https://expense-tracker-api-qbvy.onrender.com", {
+        const response = await fetch("https://expense-tracker-api-qbvy.onrender.com/chat", {
 
             method: "POST",
 
